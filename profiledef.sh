be4c1da5abd054c9f0beca2b513a140ae4a6e3da
b08x <rwpannick@gmail.com>
2023-05-26 02:01:03 -0400

    Added the install_cachyos_repo function to install the CachyOS repository and keys. Refactored the install_check_bg function to include the installation of the CachyOS repository. Added additional functions: check_supported_isa_level, check_if_repo_was_added, check_if_repo_was_commented, add_specific_repo to handle checking and adding the CachyOS repository. Updated the install_background function to call install_cachyos_repo and install the CachyOS packages. Updated the script to check for updates from a new URL (https://gitlab.com/syncopatedlinux/iso/-/raw/main/airootfs/usr/local/bin/installer). Improved code readability and organization.

--- profiledef.sh
+++ profiledef.sh
@@ -31,5 +31,4 @@ file_permissions=(
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/usr/local/bin/installer"]="0:0:755"
-  ["/usr/local/bin/cachyos-repo.sh"]="0:0:755"
 )
