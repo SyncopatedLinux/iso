d7c9440665bcc39ce859be0a7c70a19748d95d50
b08x <rwpannick@gmail.com>
2023-05-26 02:37:31 -0400

    dded a SharedPrompt class to provide a shared prompt object for the vCPUs and memory prompts. Updated the QemuSystemCommand and VirtInstallCommand classes to accept vCPUs and memory as parameters.

--- run_test_vm.rb
+++ run_test_vm.rb
@@ -10,14 +10,47 @@ class QemuCommand
   end
 end
 
+class SharedPrompt
+  def self.prompt
+    @prompt ||= TTY::Prompt.new
+  end
+end
+
 class QemuSystemCommand < QemuCommand
-  def initialize(selected_iso, selected_drive)
+  def initialize(selected_iso, selected_drive,vcpus,memory)
     @selected_iso = selected_iso
     @selected_drive = selected_drive
+    @vcpus = vcpus
+    @memory = memory
   end
 
   def execute
-    command = "qemu-system-x86_64 -cdrom #{@selected_iso} -cpu host -enable-kvm -m 2048 -smp 2 -drive file=#{@selected_drive},format=qcow2"
+    command = "qemu-system-x86_64 -cdrom #{@selected_iso} -cpu host -enable-kvm -m #{memory} -smp #{vcpus} -drive file=#{@selected_drive},format=qcow2"
+    puts "Executing command: #{command}"
+
+    # Fork the process to execute the command
+    pid = fork do
+      exec(command)
+    end
+
+    # Wait for the child process to finish
+    Process.wait(pid)
+  end
+end
+
+class VirtInstallCommand < QemuCommand
+  def initialize(selected_iso, selected_drive,vcpus,memory)
+    @selected_iso = selected_iso
+    @selected_drive = selected_drive
+    @vcpus = vcpus
+    @memory = memory
+  end
+
+  def execute
+    vcpus = prompt.ask('Enter the number of vCPUs:')
+    memory = prompt.ask('Enter the memory size (in MB):', convert: :int)
+
+    command = "virt-install --cdrom #{@selected_iso} --disk #{@selected_drive} --cpu host --memory #{memory} --vcpus #{vcpus}"
     puts "Executing command: #{command}"
 
     # Fork the process to execute the command
@@ -61,6 +94,7 @@ drive_files = Dir.glob(File.join(drive_folder, '*.qcow2')).sort
 prompt = TTY::Prompt.new
 
 create_new_disk_choice = 'Create new QEMU disk'
+
 choices = iso_files.map.with_index do |iso_file, index|
   iso_name = File.basename(iso_file)
   drive_name = File.basename(drive_files[index]) if drive_files[index]
@@ -84,7 +118,10 @@ if selection == create_new_disk_choice
   selected_iso = Shellwords.escape(prompt.select('Select ISO file:', iso_files))
   selected_drive = Shellwords.escape(prompt.select('Select drive file:', drive_files))
 
-  command = QemuSystemCommand.new(selected_iso, selected_drive)
+  vcpus = SharedPrompt.prompt.ask('Enter the number of vCPUs:')
+  memory = SharedPrompt.prompt.ask('Enter the memory size (in MB):', convert: :int)
+
+  command = QemuSystemCommand.new(selected_iso, selected_drive, vcpus, memory)
   command.execute
 else
   choice_index = selection.split(')')[0].to_i - 1
@@ -92,6 +129,9 @@ else
   selected_iso = Shellwords.escape(iso_files[choice_index])
   selected_drive = Shellwords.escape(drive_files[choice_index])
 
-  command = QemuSystemCommand.new(selected_iso, selected_drive)
+  vcpus = SharedPrompt.prompt.ask('Enter the number of vCPUs:')
+  memory = SharedPrompt.prompt.ask('Enter the memory size (in MB):', convert: :int)
+
+  command = QemuSystemCommand.new(selected_iso, selected_drive, vcpus, memory)
   command.execute
 end
